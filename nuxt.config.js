8ce41d2061cdcfe632d47d1eec5d8308e18ad856
OAT <teepakorn.oat@gmail.com>
2024-03-29 17:10:53 +0700

    Uploaded Project

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  ssr: false,
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'เว็บไซต์สำหรับการตรวจสอบค่าฝุ่นละอองในอากาศในจังหวัดเชียงใหม่',
+    htmlAttrs: {
+      lang: 'th'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/icon-app.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+      '@/assets/css/font.css',
+      'primeflex/primeflex.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '@/plugins/vuetyped.js', ssr: false }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // Doc: https://www.primefaces.org/primevue/showcase-v2/#/setup
+    [
+      'primevue/nuxt', {
+          theme: 'bootstrap4-dark-blue',      //name of the theme, defaults to saga-blue
+          ripple: true,    //whether the ripple animation is enabled, defaults to false
+          components: ['Button',],     //an array of components to be registered
+          directives: []      //an array of directives to be registered
+      }
+  ],
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    // https://github.com/primefaces/primevue/issues/844
+    transpile: ['primevue'],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="modal">
+      <vue-typed-js
+        :strings="['เว็บไซต์ตรวจสอบค่าฝุ่นละอองในอากาศในจังหวัดเชียงใหม่']"
+      >
+        <h2 class="typing"></h2>
+      </vue-typed-js>
+
+      <div class="text">
+        <div>
+          <p>สนับสนุนด้านข้อมูลโดย</p>
+          <p>บริษัท โทรคมนาคมแห่งชาติ จำกัด (มหาชน)</p>
+          <img
+            src="../assets/img/01_NT-Logo.png"
+            alt="NT Logo"
+            width="100"
+            height="65"
+          />
+        </div>
+
+        <a href="/home">
+          <button class="button">เข้าสู่เว็บไซต์</button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "ยินดีต้อนรับ",
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../assets/img/bg2.jpg);
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.modal {
+  max-width: 50%;
+  padding: 1rem 2rem;
+  -webkit-backdrop-filter: blur(15px);
+  backdrop-filter: blur(15px);
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 0.25rem;
+}
+
+.text {
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: 600;
+}
+
+.button {
+  font-family: "Athiti", sans-serif;
+  font-weight: 500;
+  border: none;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 20px;
+  cursor: pointer;
+  background-color: transparent;
+  color: black;
+  border: 2px solid #555555;
+  transition: 1s;
+}
+.button:hover {
+  background-color: black;
+  color: white;
+}
+</style>
